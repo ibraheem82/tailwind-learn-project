8d4dea2087ee51105fc302bde79cc650131234b4
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-01-27 10:32:32 +0100

    my tailwind project is now completed.........

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // darkMode: 'media',
+  darkMode: 'class', //  This tells Tailwind that you'll be controlling dark mode using a CSS class.
+  content: ["*"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
